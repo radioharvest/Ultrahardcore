6c3d54e8bcfbdb18e2f2ad0f4a1cd06e7f19fc68
Robert Khalikov <nbjk667@gmail.com>
2015-10-17 14:13:37 -0500

    Added start scene
    Scene contains persistent object. Child objects can be added to it in
    order to keep them alive when new scene is loaded.

---
+++ Assets/Scripts/Persistent.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Persistent : MonoBehaviour
+{
+	void Start()
+	{
+		DontDestroyOnLoad(this);
+
+		// Make children persistent
+		int numChildren = transform.childCount;
+		for (int i = 0; i < numChildren; i++)
+		{
+			Transform childTransform = transform.GetChild(i);
+			GameObject childGameObject = childTransform.gameObject;
+			if (childGameObject != null)
+			{
+				DontDestroyOnLoad(childGameObject);
+			}
+		}
+	}
+}

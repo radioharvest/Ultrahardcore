10254411bc1c6c45aec7033bfb2db20aae1e5742
Robert Khalikov <nbjk667@gmail.com>
2015-10-17 22:16:33 -0500

    Added persistent data accessors

--- Assets/Scripts/Persistent/Persistent.cs
+++ Assets/Scripts/Persistent/Persistent.cs
@@ -30,4 +30,49 @@ public class Persistent : MonoBehaviour
 			RecursivelyMakePersistent(tr.GetChild(i));
 		}
 	}
+
+	// Preferences accessor
+	static Preferences m_Preferences;
+	static bool m_PreferencesNotFound = false;
+	public static Preferences PersistentPreferences
+	{
+		get
+		{
+			if (m_Preferences == null && !m_PreferencesNotFound)
+			{
+				m_Preferences = FindGlobalComponentByName<Preferences>("Preferences");
+				m_PreferencesNotFound = (m_Preferences == null);
+			}
+
+			return m_Preferences;
+		}
+	}
+
+	// Game data accessor
+	static GameData m_GameData;
+	static bool m_GameDataNotFound = false;
+	public static GameData PersistentData
+	{
+		get
+		{
+			if (m_GameData == null && !m_GameDataNotFound)
+			{
+				m_GameData = FindGlobalComponentByName<GameData>("Data");
+				m_GameDataNotFound = (m_GameData == null);
+			}
+
+			return m_GameData;
+		}
+	}
+
+	static T FindGlobalComponentByName<T>(string name)
+	{
+		GameObject go = GameObject.Find(name);
+		if (go != null)
+		{
+			return go.GetComponent<T>();
+		}
+
+		return default(T);
+	}
 }
